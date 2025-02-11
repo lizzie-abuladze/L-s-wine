cf990194e4aa0b971aacdd7f4eee4350decfddb1
lizzie-abuladze <liziabuladze54@gmail.com>
2025-02-11 14:58:10 +0400

    wine-site-frontend

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-let btnElement = document.getElementById("button");
+let btnElement = document.getElementById("buttonEl");
 btnElement.addEventListener('click' , function(){
     alert("Awesome! Your news will be delivered shortly.");
+})
+let navElement = document.getElementById("navBar");
+let burgerElement = document.getElementById("burgerBar");
+
+burgerElement.addEventListener('click' , function(){
+    navElement.classList.toggle("activeNav");
 })
